d3daaa7453a9aa790c0a89e222db4852f50c3dda
wroncall <wroncall@cuc.edu.co>
2024-10-17 12:07:03 -0500

    hasta Inicio

--- src/app/componentes/contenedor/Cabecera.tsx
+++ src/app/componentes/contenedor/Cabecera.tsx
@@ -1,8 +1,90 @@
+import { NavLink } from "react-router-dom";
+import miLogo from "../../../assets/img/logoReact.png"
+
 export const Cabecera = () => {
-    return (
-        <>
-        Hola soy la cabecera <br />
-        El menu de arriba
-        </>
-    )
-}
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            < img src = {miLogo} alt= "El Logo"/>
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor02"
+            aria-controls="navbarColor02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor02">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Inicio
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="*">
+                  Franquicia
+                </NavLink>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Menus
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <NavLink className="dropdown-item" to="/mencre">
+                      Registrar Menu
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/menlis">
+                      Listar Menu
+                    </NavLink>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/menadmin">
+                      Administrar Menu
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/menuacer">
+                  Acerca De
+                </NavLink>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-light" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,9 +1,21 @@
 export const Inicio = () => {
     return (
-        <>
-        <br /><br/>
-        Soy el inicio <br />
-        Como el de bienvenida
-        </>
-    )
-}
+      <>
+        <div className="pt-5 d-flex justify-content-center">
+          <div className="col-md-6">
+            <div className="h-100 p-5 text-bg-dark rounded-3">
+              <h2>Change the background</h2>
+              <p>
+                Swap the background-color utility and add a .text-* color
+                utility to mix up the jumbotron look. Then, mix and match with
+                additional component themes and more.
+              </p>
+              <button className="btn btn-outline-light" type="button">
+                Example button
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
